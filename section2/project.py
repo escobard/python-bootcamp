ccab5d6b208694c349523ae44ec5aae2e4a6f065
escobard <escobard90@gmail.com>
2023-11-27 08:26:50 -0700

    section2: project improved with instructor solution

--- section2/project.py
+++ section2/project.py
@@ -7,12 +7,13 @@
 
 #Write your code below this line 👇
 print("Welcome to the tip calculator.")
-total_bill = input("What was the total bill?")
-tip_percentage = input("What tip percentage would you like to give?")
-how_many = input("How many people to split the bill?")
-total_bill = int(total_bill)
-tip_percentage = int(tip_percentage)
-how_many = int(how_many)
+
+## can pre-define type of input by assigning a type before input declaration
+total_bill = float(input("What was the total bill? $"))
+tip_percentage = int(input("What tip percentage would you like to give?"))
+how_many = int(input("How many people to split the bill?"))
 bill_with_tip = total_bill + (total_bill * tip_percentage / 100)
 bill_per_person = bill_with_tip / how_many
+## .2f - We can use the %f formatter to specify the number of decimal numbers \
+## to be returned when a floating point number is rounded up.
 print(f"Each person should pay: ${bill_per_person:.2f}")
