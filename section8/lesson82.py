2d50cb2363733e892bd6133612ffb7c8edd2bf69
escobard <escobard90@gmail.com>
2023-12-03 10:44:22 -0700

    section8, lesson82

---
+++ section8/lesson82.py
@@ -0,0 +1,13 @@
+# create a function called greet()
+# print 3 statements inside the function
+# call the function
+def greet():
+  print("1")
+  print("2")
+  print("3")
+greet()
+
+# create a function with an input and print the input
+def greet_with_input(name):
+  print(f"Your name is: {name}")
+greet_with_input('Daniel')
