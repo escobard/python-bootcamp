b604de3c4602e797e4e079bbf3d82da16beae17b
escobard <escobard90@gmail.com>
2023-12-03 08:49:36 -0700

    section7, lesson 72, improved hangman loop algo with range

--- section7_hangman/lesson72.py
+++ section7_hangman/lesson72.py
@@ -1,7 +1,7 @@
 #Step 2
 
 import random
-word_list = ["aardvark", "baboon", "camel"]
+word_list = ["aardvark", "baboon", "camel", "crocodile", "gorilla", "donkey"]
 chosen_word = random.choice(word_list)
 
 #Testing code
@@ -20,14 +20,13 @@ guess = input("Guess a letter: ").lower()
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-counter = 0
-for letter in chosen_word:
+for position in range(len(chosen_word)):
+    letter = chosen_word[position]
     if letter == guess:
-        display[counter] = guess
+        display[position] = guess
         print("Right")
     else:
         print("Wrong")
-    counter += 1
 
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
