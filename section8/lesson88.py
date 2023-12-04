4a4e8c38687bc39f61c538cbb1f894ca95bd1881
escobard <escobard90@gmail.com>
2023-12-04 11:18:34 -0700

    section8: project starting code

--- section8/lesson88.py
+++ section8/lesson88.py
@@ -9,14 +9,15 @@ shift = int(input("Type the shift number:\n"))
 def caesar(direction, text, shift):
   cypher_text = ""
   for char in text:
-      if direction == 'encode':
-        # handle cases where shift takes index beyond alphabet length
-        if alphabet.index(char) + shift > 25:
-          shift_char = alphabet[(alphabet.index(char) + shift) - 26]
-        else:
-          shift_char = alphabet[alphabet.index(char) + shift]
-      elif direction == 'decode':
-        shift_char = alphabet[alphabet.index(char) - shift]
+    if direction == 'encode':
+      # handle cases where shift takes index beyond alphabet length
+      if alphabet.index(char) + shift > 25:
+        shift_char = alphabet[(alphabet.index(char) + shift) - 26]
+      else:
+        shift_char = alphabet[alphabet.index(char) + shift]
+    ## no need to protect loop for negative index values, as negative index values are valid in python!
+    elif direction == 'decode':
+      shift_char = alphabet[alphabet.index(char) - shift]
     cypher_text += shift_char
   print(f"The {direction} text is {cypher_text}")
 
