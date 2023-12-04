e13aa625932c79fc7a870f08d5d3e2866ef45ba8
escobard <escobard90@gmail.com>
2023-12-04 10:40:25 -0700

    section8: lesson87 starting code

--- section8/lesson86.py
+++ section8/lesson86.py
@@ -6,7 +6,7 @@ shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(text, shift):
-  cypher_text = []
+  cypher_text = ""
   #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
   #e.g. 
   #plain_text = "hello"
@@ -27,7 +27,7 @@ def encrypt(text, shift):
   #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
 
   ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-  print(f"The encoded text is {''.join(cypher_text)}")
+  print(f"The encoded text is {cypher_text}")
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
 encrypt(text, shift)
