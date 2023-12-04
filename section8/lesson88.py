6513554d87445706ea54c9bfa342f50d5d07c7e6
escobard <escobard90@gmail.com>
2023-12-04 11:09:35 -0700

    section8: lesson88 solution

--- section8/lesson88.py
+++ section8/lesson88.py
@@ -5,26 +5,19 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 # TODO - combine encrypt() and decrypt() functions into a single function called caesar()
-def encrypt(text, shift):
+
+def caesar(direction, text, shift):
   cypher_text = ""
   for char in text:
-    # handle cases where shift takes index beyond alphabet length
-    if alphabet.index(char) + shift > 25:
-      shift_char = alphabet[(alphabet.index(char) + shift) - 26]
-    else:
-      shift_char = alphabet[alphabet.index(char) + shift]
+      if direction == 'encode':
+        # handle cases where shift takes index beyond alphabet length
+        if alphabet.index(char) + shift > 25:
+          shift_char = alphabet[(alphabet.index(char) + shift) - 26]
+        else:
+          shift_char = alphabet[alphabet.index(char) + shift]
+      elif direction == 'decode':
+        shift_char = alphabet[alphabet.index(char) - shift]
     cypher_text += shift_char
-  print(f"The encoded text is {cypher_text}")
+  print(f"The {direction} text is {cypher_text}")
 
-def decrypt(text, shift):
-  decrypted_text = ""
-  for char in text:
-    ## no need to protect loop for negative index values, as negative index values are valid in python!
-    shift_char = alphabet[alphabet.index(char) - shift]
-    decrypted_text += shift_char
-  print(f"The decoded text is {decrypted_text}")
-
-if direction == 'encode':
-  encrypt(text, shift)
-elif direction == 'decode':
-  decrypt(text, shift)
+caesar(direction, text, shift)
