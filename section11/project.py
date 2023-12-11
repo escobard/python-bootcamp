6c10f31175341121a491ec467c4e0919efd8d174
escobard <escobard90@gmail.com>
2023-12-11 12:53:48 -0700

    section11: blackjack with working draw / bust functionality

--- section11/project.py
+++ section11/project.py
@@ -42,27 +42,26 @@ logo = """
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 is_running = True
+is_drawing = True
 
-def checkWinner(your_cards, computer_cards):
+def checkWinner(your_cards, computer_cards, is_drawing):
 
   print(f"Your Cards {your_cards}, current score: {sum(your_cards)}")
   print(f"Computer score is {sum(computer_cards)}")
   
+  if sum(computer_cards) > 21:
+    print(f"Bust - Computer loses, you win!")
   if sum(your_cards) > 21:
     print(f"Bust - You lose!")
-    is_running = False  
-  elif sum(computer_cards) > 21:
-    print(f"Bust - Computer loses, you win!")
-    is_running = False
-  elif sum(your_cards) == 21:
-    print(f"Blackjack - you win!")
+  elif sum(your_cards) == 21 and is_drawing == False:
+    print(f"Blackjack - you win!") 
   elif sum(computer_cards) == 21:
     print(f"Blackjack - Computer wins!")
-  elif sum(your_cards) > sum(computer_cards):
+  elif sum(your_cards) > sum(computer_cards) and is_drawing == False:
     print(f"You win!")
-  elif sum(your_cards) < sum(computer_cards):
+  elif sum(your_cards) < sum(computer_cards) and is_drawing == False:
     print(f"Computer wins!")
-  elif sum(your_cards) == sum(computer_cards):
+  elif sum(your_cards) == sum(computer_cards) and is_drawing == False:
     print(f"It's a tie!")
 
 while is_running:
@@ -78,26 +77,23 @@ while is_running:
 
     print(f"Your Cards {your_cards}, current score: {sum(your_cards)}")
     print(f"Computer's first card {computer_cards[0]}")
-
-    if sum(your_cards) == 21:
-      print(f"Blackjack - You win!")
-      is_running = False
-    elif sum(your_cards) > 21:
-      print(f"Bust - You lose!")
-      is_running = False
     
-    ## can only draw once in this version of the game
-    draw = input("Type 'y' to get another card, type 'n' to pass: ")
-    if draw == 'n':
-
-      computer_cards.append(random.choice(cards))
-      checkWinner(your_cards, computer_cards)
-      is_running = False
-    else:
-      computer_cards.append(random.choice(cards))
-      your_cards.append(random.choice(cards))
-      checkWinner(your_cards, computer_cards)
-      is_running = False  
+    while is_drawing == True and is_running == True:
+      # breaks the inner while loop if player or computer busts
+      if sum(computer_cards) >= 22 or sum(your_cards) >= 22:
+        is_drawing = False 
+        is_running = False 
+        break
+      draw = input("Type 'y' to get another card, type 'n' to pass: ")
+      if draw == 'n':
+        is_drawing = False
+        computer_cards.append(random.choice(cards))
+        checkWinner(your_cards, computer_cards, is_drawing)
+        is_running = False
+      else:
+        computer_cards.append(random.choice(cards))
+        your_cards.append(random.choice(cards))
+        checkWinner(your_cards, computer_cards, is_drawing)
   else:
     is_running = False
     print('Thanks for playing!')
