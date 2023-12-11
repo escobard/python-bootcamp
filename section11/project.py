521c33113b52f704f91867c8491a462608990568
escobard <escobard90@gmail.com>
2023-12-11 13:05:51 -0700

    section11: blackjack recursion and logic cleanup, working as expected!

--- section11/project.py
+++ section11/project.py
@@ -41,9 +41,6 @@ logo = """
 ## game does not actually remove cards once chosen, but could be improved in the future
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-is_running = True
-is_drawing = True
-
 def checkWinner(your_cards, computer_cards, is_drawing):
 
   print(f"Your Cards {your_cards}, current score: {sum(your_cards)}")
@@ -55,7 +52,7 @@ def checkWinner(your_cards, computer_cards, is_drawing):
     print(f"Bust - You lose!")
   elif sum(your_cards) == 21 and is_drawing == False:
     print(f"Blackjack - you win!") 
-  elif sum(computer_cards) == 21:
+  elif sum(computer_cards) == 21 and is_drawing == False:
     print(f"Blackjack - Computer wins!")
   elif sum(your_cards) > sum(computer_cards) and is_drawing == False:
     print(f"You win!")
@@ -64,36 +61,48 @@ def checkWinner(your_cards, computer_cards, is_drawing):
   elif sum(your_cards) == sum(computer_cards) and is_drawing == False:
     print(f"It's a tie!")
 
-while is_running:
-  print(logo)
-  play = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
-  if play == 'y':
-    your_cards = []
-    your_cards.append(random.choice(cards))
-    your_cards.append(random.choice(cards))
 
-    computer_cards = []
-    computer_cards.append(random.choice(cards))
+def blackjack():
+  
+  is_running = True
+  is_drawing = True
 
-    print(f"Your Cards {your_cards}, current score: {sum(your_cards)}")
-    print(f"Computer's first card {computer_cards[0]}")
-    
-    while is_drawing == True and is_running == True:
-      # breaks the inner while loop if player or computer busts
-      if sum(computer_cards) >= 22 or sum(your_cards) >= 22:
-        is_drawing = False 
-        is_running = False 
-        break
-      draw = input("Type 'y' to get another card, type 'n' to pass: ")
-      if draw == 'n':
-        is_drawing = False
-        computer_cards.append(random.choice(cards))
-        checkWinner(your_cards, computer_cards, is_drawing)
-        is_running = False
-      else:
-        computer_cards.append(random.choice(cards))
-        your_cards.append(random.choice(cards))
-        checkWinner(your_cards, computer_cards, is_drawing)
-  else:
-    is_running = False
-    print('Thanks for playing!')
+  while is_running:
+    print(logo)
+    play = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+    if play == 'y':
+      your_cards = []
+      your_cards.append(random.choice(cards))
+      your_cards.append(random.choice(cards))
+
+      computer_cards = []
+      computer_cards.append(random.choice(cards))
+
+      print(f"Your Cards {your_cards}, current score: {sum(your_cards)}")
+      print(f"Computer's first card {computer_cards[0]}")
+      
+      while is_drawing == True and is_running == True:
+        # breaks the inner while loop if player or computer busts
+        if sum(computer_cards) >= 22 or sum(your_cards) >= 22:
+          is_drawing = False 
+          is_running = False 
+          blackjack()
+          break
+        draw = input("Type 'y' to get another card, type 'n' to pass: ")
+        if draw == 'n':
+          is_drawing = False
+          computer_cards.append(random.choice(cards))
+          checkWinner(your_cards, computer_cards, is_drawing)
+        else:
+          computer_cards.append(random.choice(cards))
+          your_cards.append(random.choice(cards))
+          checkWinner(your_cards, computer_cards, is_drawing)
+    elif play == 'n':
+      is_running = False
+      print('Thanks for playing!')
+    else:
+      print('Unknown answer. Thanks for playing!')
+      # calls blackjack function so that it can re-set the program
+      blackjack()
+
+blackjack()
