434c145211194d59f0a2bbcff1e1e354f1228586
escobard <escobard90@gmail.com>
2023-12-12 19:53:07 -0700

    feat: add process_coins function

--- section15/coffee_machine.py
+++ section15/coffee_machine.py
@@ -31,13 +31,16 @@ resources = {
 }
 
 is_running = True
+processing_coins = True
 
 
 def check_resources(resources_to_check, coffee_choice):
   global is_running
+
   choice = menu[coffee_choice]
   out_of_ingredients = []
   has_ingredients = True
+
   for ingredient in choice['ingredients']:
     # print(f"Ingredient: {choice['ingredients'][ingredient]}")
     # print(f"Resource to check: {resources_to_check[ingredient]}")
@@ -45,24 +48,51 @@ def check_resources(resources_to_check, coffee_choice):
       print(f"Sorry there is not enough {ingredient}")
       out_of_ingredients.append(ingredient)
       has_ingredients = False
-  ## simplified syntax for false statements, nice!
+
+  ## simplified syntax for false boolean checks, nice!
   if not has_ingredients:
     print(f"Machine shut down, not enough {''.join(out_of_ingredients)}")
     is_running = False
 
+def process_coins(cost):
+  global processing_coins
+
+  print('Please insert coins.')
+
+  quarters = int(input('How many Quarters?'))
+  dimes = int(input('How many Dimes?'))
+  nickles = int(input('How many Nickles?'))
+  pennies = int(input('How many Pennies?'))
+  total = (quarters * 0.25) + (dimes * 0.1) + (nickles * 0.05) + (pennies * 0.01)
+
+  if cost > total:
+    print(f"Sorry, ${total} is not enough money. Money refunded")
+    processing_coins = False
+  else:
+    if total > cost:
+      print(f'Your change is {total - cost}')
+
 
 def coffee_machine():
-  ## leverage global variable
+  ## leverage global variables
   global is_running
+  global processing_coins
+
   available_resources = resources
+
   while is_running:
     user_choice = input('What would you like? (espresso/latte/cappuccino):')
+
     if user_choice == 'off':
       is_running = False
     elif user_choice == 'print':
       print(f'Available resources: {available_resources}')
     else:
-      print(user_choice)
+      print(f"You have selected a {user_choice}")
       check_resources(available_resources, user_choice)
+
+      while processing_coins and is_running:
+        process_coins(menu[user_choice]['cost'])
+
 
 coffee_machine()
