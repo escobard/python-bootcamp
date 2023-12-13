7a6f2112ff8060017c5a38c38070f0f129fe9a64
escobard <escobard90@gmail.com>
2023-12-12 19:39:06 -0700

    feat: add check_resources function to check if there is enough ingredients

--- section15/coffee_machine.py
+++ section15/coffee_machine.py
@@ -30,8 +30,30 @@ resources = {
   "coffee": 100,
 }
 
+is_running = True
+
+
+def check_resources(resources_to_check, coffee_choice):
+  global is_running
+  choice = menu[coffee_choice]
+  out_of_ingredients = []
+  has_ingredients = True
+  for ingredient in choice['ingredients']:
+    # print(f"Ingredient: {choice['ingredients'][ingredient]}")
+    # print(f"Resource to check: {resources_to_check[ingredient]}")
+    if choice['ingredients'][ingredient] > resources_to_check[ingredient]:
+      print(f"Sorry there is not enough {ingredient}")
+      out_of_ingredients.append(ingredient)
+      has_ingredients = False
+  ## simplified syntax for false statements, nice!
+  if not has_ingredients:
+    print(f"Machine shut down, not enough {''.join(out_of_ingredients)}")
+    is_running = False
+
+
 def coffee_machine():
-  is_running = True
+  ## leverage global variable
+  global is_running
   available_resources = resources
   while is_running:
     user_choice = input('What would you like? (espresso/latte/cappuccino):')
@@ -41,6 +63,6 @@ def coffee_machine():
       print(f'Available resources: {available_resources}')
     else:
       print(user_choice)
-
+      check_resources(available_resources, user_choice)
 
 coffee_machine()
