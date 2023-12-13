e9b49b9121023eff0e78f151ab8e5cc2ee567835
escobard <escobard90@gmail.com>
2023-12-12 20:00:08 -0700

    refact: simplify functions and arguments with global variables

--- section15/coffee_machine.py
+++ section15/coffee_machine.py
@@ -34,17 +34,17 @@ is_running = True
 processing_coins = True
 
 
-def check_resources(resources_to_check, coffee_choice):
+def check_resources(coffee_choice):
   global is_running
+  global resources
 
-  choice = menu[coffee_choice]
   out_of_ingredients = []
   has_ingredients = True
 
-  for ingredient in choice['ingredients']:
+  for ingredient in coffee_choice['ingredients']:
     # print(f"Ingredient: {choice['ingredients'][ingredient]}")
     # print(f"Resource to check: {resources_to_check[ingredient]}")
-    if choice['ingredients'][ingredient] > resources_to_check[ingredient]:
+    if coffee_choice['ingredients'][ingredient] > resources[ingredient]:
       print(f"Sorry there is not enough {ingredient}")
       out_of_ingredients.append(ingredient)
       has_ingredients = False
@@ -70,29 +70,31 @@ def process_coins(cost):
     processing_coins = False
   else:
     if total > cost:
-      print(f'Your change is {total - cost}')
+      print(f'Your change is {round(total - cost, 2)}')
+
+def process_transaction():
+  global resources
 
 
 def coffee_machine():
   ## leverage global variables
   global is_running
   global processing_coins
-
-  available_resources = resources
+  global resources
 
   while is_running:
     user_choice = input('What would you like? (espresso/latte/cappuccino):')
-
+    coffee_choice = menu[user_choice]
     if user_choice == 'off':
       is_running = False
     elif user_choice == 'print':
-      print(f'Available resources: {available_resources}')
+      print(f'Available resources: {resources}')
     else:
       print(f"You have selected a {user_choice}")
-      check_resources(available_resources, user_choice)
+      check_resources(coffee_choice)
 
       while processing_coins and is_running:
-        process_coins(menu[user_choice]['cost'])
-
+        process_coins(coffee_choice['cost'])
+        process_transaction()
 
 coffee_machine()
