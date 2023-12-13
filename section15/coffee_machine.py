74f59028459ae5339290c6b79f1ae78bf157cfd0
escobard <escobard90@gmail.com>
2023-12-12 18:51:49 -0700

    feat: add coffee_machine function with base prompts

--- section15/coffee_machine.py
+++ section15/coffee_machine.py
@@ -1,32 +1,46 @@
 menu = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
+  "espresso": {
+    "ingredients": {
+      "water": 50,
+      "coffee": 18,
     },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
+    "cost": 1.5,
+  },
+  "latte": {
+    "ingredients": {
+      "water": 200,
+      "milk": 150,
+      "coffee": 24,
     },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
+    "cost": 2.5,
+  },
+  "cappuccino": {
+    "ingredients": {
+      "water": 250,
+      "milk": 100,
+      "coffee": 24,
+    },
+    "cost": 3.0,
+  }
 }
 
 resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
+  "water": 300,
+  "milk": 200,
+  "coffee": 100,
 }
 
+def coffee_machine():
+  is_running = True
+  available_resources = resources
+  while is_running:
+    user_choice = input('What would you like? (espresso/latte/cappuccino):')
+    if user_choice == 'off':
+      is_running = False
+    elif user_choice == 'print':
+      print(f'Available resources: {available_resources}')
+    else:
+      print(user_choice)
+
+
+coffee_machine()
