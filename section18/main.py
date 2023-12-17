b59913684643722d14822c310e501c095842f56c
escobard <escobard90@gmail.com>
2023-12-17 12:09:26 -0700

    section18: lessons 167, add draw square with turtle, simplified with for loop!

--- section18/main.py
+++ section18/main.py
@@ -4,13 +4,9 @@ timmy_the_turtle = Turtle()
 timmy_the_turtle.shape('turtle')
 timmy_the_turtle.color('green')
 
-timmy_the_turtle.forward(200)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(200)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(200)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(200)
+for _ in range(4):
+  timmy_the_turtle.forward(200)
+  timmy_the_turtle.right(90)
 
 screen = Screen()
 screen.exitonclick()
