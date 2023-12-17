8c8e19db2f4155c065feccceca8afe576c51d20d
escobard <escobard90@gmail.com>
2023-12-17 12:04:41 -0700

    section18: lessons 165-166, add turtle game main file

---
+++ section18/main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape('turtle')
+timmy_the_turtle.color('green')
+
+timmy_the_turtle.right(100)
+
+screen = Screen()
+screen.exitonclick()
