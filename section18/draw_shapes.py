85911005d894c7c06bdbe8bd16ce61921ba59339
escobard <escobard90@gmail.com>
2023-12-18 11:55:07 -0700

    section18: lessons 170, add random colors to shapes

--- section18/draw_shapes.py
+++ section18/draw_shapes.py
@@ -2,10 +2,13 @@
 ## https://docs.python.org/3/library/turtle.html#introduction
 ### draw a triangle, square, pentagon, hexagon, heptagon, octagon, nonagon and decagon
 from turtle import Turtle, Screen
+import random
 
 arrow = Turtle()
 arrow.shape('arrow')
 arrow.color('black')
+
+colors = ['GreenYellow', 'Chocolate', 'Gold', 'LightSeaGreen', 'SkyBlue', 'MediumBlue', 'DodgerBlue', 'Brown', 'LightSkyBlue']
 
 def draw_shape(num_sides):
   angle = 360 / num_sides
@@ -16,4 +19,5 @@ def draw_shape(num_sides):
 # runs loop from numbers 3-10
 ## starts the loop at triangle (3 sides), to square (4 sides) and so on
 for shape_side_n in range (3, 11):
+  arrow.color(random.choice(colors))
   draw_shape(shape_side_n)
