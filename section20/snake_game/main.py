7d3562ba86ce77f04596ab204f31ccde823fd3a2
escobard <escobard90@gmail.com>
2024-01-02 17:09:05 -0700

    section20: lesson 185, add 3 squares to represent snake

--- section20/snake_game/main.py
+++ section20/snake_game/main.py
@@ -7,5 +7,12 @@ screen.bgcolor('black')
 # add a title to application screen
 screen.title('Snake Game')
 
+starting_positions = [(0,0), (-20, 0), (-40, 0)]
+
+for position in starting_positions:
+  new_segment = Turtle('square')
+  new_segment.color('white')
+  new_segment.goto(position)
+
 
 screen.exitonclick()
