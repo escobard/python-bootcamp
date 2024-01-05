ce3dd78a9228a85286c2c9e1773912d8788ecc86
escobard <escobard90@gmail.com>
2024-01-04 19:31:28 -0700

    section22: lesson 202, add a ball class and apply to main.py

--- section22/pong_game/main.py
+++ section22/pong_game/main.py
@@ -1,6 +1,8 @@
+import time
 from turtle import Screen
 
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.bgcolor('black')
@@ -11,6 +13,7 @@ screen.tracer(0)
 
 right_paddle = Paddle((350, 0))
 left_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(right_paddle.go_up, 'Up')
@@ -20,6 +23,8 @@ screen.onkey(left_paddle.go_down, 's')
 
 game_is_on = True
 while game_is_on:
+  time.sleep(0.1)
   screen.update()
+  ball.move()
 
 screen.exitonclick()
