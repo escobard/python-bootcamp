4ae27f252095c4e1e88656c1a3c814f546e697bc
escobard <escobard90@gmail.com>
2024-01-05 12:51:35 -0700

    section22: lesson 205, detect when paddle misses and reset ball position

--- section22/pong_game/main.py
+++ section22/pong_game/main.py
@@ -3,6 +3,7 @@ from turtle import Screen
 
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor('black')
@@ -14,6 +15,7 @@ screen.tracer(0)
 right_paddle = Paddle((350, 0))
 left_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(right_paddle.go_up, 'Up')
@@ -28,12 +30,23 @@ while game_is_on:
   ball.move()
 
   #Detect collision with top/bottom wall
-  ## ignores left/right wall, as that will score points for the opposite paddle
+  ## ignores left/right wall, as that will reset the game
   if ball.ycor() > 280 or ball.ycor() < -280:
     ball.bounce_y()
 
   # detect collision with paddles
-  if ball.distance(right_paddle) < 50 and ball.xcor() > 340 or ball.distance(left_paddle) < 50 and ball.xcor() > -340:
+  if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
     ball.bounce_x()
+
+  # detect right paddle misses
+  if ball.xcor() > 380:
+    ball.reset_position()
+    scoreboard.left_point()
+
+  # detect if left paddle misses
+  if ball.xcor() < -380:
+    ball.reset_position()
+    scoreboard.right_point()
+
 
 screen.exitonclick()
