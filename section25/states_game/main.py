7e0952404e95c87e82cc06f37e94a22341838aa2
escobard <escobard90@gmail.com>
2024-01-08 16:28:19 -0700

    section25: lesson 231, us states game, input to collect the user's answer

--- section25/states_game/main.py
+++ section25/states_game/main.py
@@ -8,4 +8,8 @@ image = 'blank_states_img.gif'
 screen.addshape(image)
 turtle.shape(image)
 
+# add an input to the turtle screen to get user answer
+answer_state = screen.textinput(title='Guess the state', prompt="What's another state's name")
+print(answer_state)
+
 screen.exitonclick()
