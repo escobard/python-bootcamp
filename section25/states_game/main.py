b654bdd9e3c19e469df98b75955d19ba1a5f4490
escobard <escobard90@gmail.com>
2024-01-09 09:54:45 -0700

    section25: lesson 232, check input to check guess from file's data

--- section25/states_game/main.py
+++ section25/states_game/main.py
@@ -1,3 +1,4 @@
+import pandas
 import turtle
 
 screen = turtle.Screen()
@@ -8,11 +9,17 @@ image = 'blank_states_img.gif'
 screen.addshape(image)
 turtle.shape(image)
 
+
+correct_guesses = []
 # add an input to the turtle screen to get user answer
+
 answer_state = screen.textinput(title='Guess the state', prompt="What's another state's name")
 answer_to_lower = answer_state.lower()
 answer = answer_to_lower.capitalize()
 
-print(answer)
+data = pandas.read_csv('50_states.csv')
+check_guess = data[data.state == answer]
+
+print(check_guess)
 
 screen.exitonclick()
