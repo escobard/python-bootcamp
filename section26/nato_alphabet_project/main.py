6fe3225966cf704f8ebef0fd21bcbc4c82215d5f
escobard <escobard90@gmail.com>
2024-01-23 20:13:49 -0700

    section26: lessons 244, row dictionary from pandas file

--- section26/nato_alphabet_project/main.py
+++ section26/nato_alphabet_project/main.py
@@ -21,7 +21,12 @@ for (index, row) in student_data_frame.iterrows():
 # {new_key:new_value for (index, row) in df.iterrows()}
 
 #TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+data = pandas.read_csv('nato_phonetic_alphabet.csv')
+
+data_to_rows = data.iterrows()
+phonetic_alphabet = {row.letter:row.code for (index, row) in data_to_rows}
+# {"A": "Alfa", "B": "Bravo"}
+print(phonetic_alphabet)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
