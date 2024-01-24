8fa0058ab578ea90ab84a742acfb859f8ce83b6d
escobard <escobard90@gmail.com>
2024-01-23 19:46:33 -0700

    section26: lessons 242, celcius to farenheit dictionary challenge

--- section26/dictionary_comprehension/number_of_letters.py
+++ section26/dictionary_comprehension/number_of_letters.py
@@ -1,6 +1,5 @@
 # You are going to use Dictionary Comprehension to create a dictionary called result that takes each word in the given sentence and calculates the number of letters in each word.
 sentence = 'What is the Airspeed Velocity of an Unladen Swallow?'
-list_of_words = sentence.split(' ')
 
-result = {word:len(word) for word in list_of_words}
+result = {word:len(word) for word in sentence.split()}
 print(result)
