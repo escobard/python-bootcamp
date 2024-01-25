cedb647526311afae5b52c510254e21ae0d46aae
escobard <escobard90@gmail.com>
2024-01-25 16:12:33 -0700

    section27: lessons 252, add tkinter button to the screen

--- section27/main.py
+++ section27/main.py
@@ -4,6 +4,9 @@
 #### tkinter was built with tk (another language), python is a wrapper around that, which is why this library uses * and ** type of arguments so freely
 import tkinter
 
+# to import all modules from a library, we can use:
+## from tkinter import *
+
 window = tkinter.Tk()
 window.title('My first GUI program')
 window.minsize(width=500, height=300)
@@ -15,8 +18,22 @@ my_label = tkinter.Label(text="I am a label", font=('Arial', 24, 'bold'))
 ## .pack() centers the tkinter object on the screen
 ### .pack() utilizes the packer, which tkinters geometry-management mechanism
 #### https://docs.python.org/3.12/library/tkinter.html#the-packer
-my_label.pack(side='left')
+my_label.pack()
 
+
+# can change configuration of objects in numerous ways after creation - https://tcl.tk/man/tcl8.6/TkCmd/entry.htm
+## eg - my_label["text"] = "New Text" - updates the text accordingly
+
+# Button
+
+## to manage events (like a click), event handlers need to be developed
+def button_clicked():
+  print("I got clicked")
+
+## command= sets the callback function in case of an event with the element
+button = tkinter.Button(text="Click Me", command=button_clicked)
+## .pack must be run to place/render every element on the screen
+button.pack()
 
 ## keeps a window on the screen with tkinter
 window.mainloop()
