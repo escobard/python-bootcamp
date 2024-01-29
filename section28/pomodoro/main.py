73cb170405686a3255a580ffd2da229c3b143a33
escobard <escobard90@gmail.com>
2024-01-29 16:37:53 -0700

    section28: lessons 257, add background to the tomato and the screen

--- section28/pomodoro/main.py
+++ section28/pomodoro/main.py
@@ -20,15 +20,18 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50)
+window.config(padx=100, pady=50, bg=YELLOW)
 
 # canvas with tkinter - allows us to draw an image on the screen
-canvas = Canvas(width=200, height=224)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 
 # photoimage - function with tkinter to read through an image file
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(103, 112, image=tomato_img)
-canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+
+# for color palettes, can try https://colorhunt.co/
+
 
 canvas.pack()
 
