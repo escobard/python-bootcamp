5f8bb5f5187b420882997263e7a70b6cc2273f1a
escobard <escobard90@gmail.com>
2024-01-29 16:57:02 -0700

    section28: lessons 258, add fg and bg color to text

--- section28/pomodoro/main.py
+++ section28/pomodoro/main.py
@@ -25,8 +25,8 @@ window.config(padx=100, pady=50, bg=YELLOW)
 # canvas with tkinter - allows us to draw an image on the screen
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 
-miles_label = Label(text="Timer")
-miles_label.grid(column=1, row=0)
+title_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 35, "bold"), bg=YELLOW)
+title_label.grid(column=1, row=0)
 
 # photoimage - function with tkinter to read through an image file
 tomato_img = PhotoImage(file="tomato.png")
@@ -36,13 +36,13 @@ canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "b
 # for color palettes, can try https://colorhunt.co/
 canvas.grid(column=1, row=1)
 
-start_button = Button(text="Start")
+start_button = Button(text="Start", highlightthickness=0)
 start_button.grid(column=0,row=2)
 
-reset_button = Button(text="Reset")
+reset_button = Button(text="Reset", highlightthickness=0)
 reset_button.grid(column=2,row=2)
 
-checkmark_label = Label(text='✓')
+checkmark_label = Label(text='✓', fg=GREEN, font=(FONT_NAME, 12), bg=YELLOW)
 checkmark_label.grid(column=1,row=3)
 
 window.mainloop()
