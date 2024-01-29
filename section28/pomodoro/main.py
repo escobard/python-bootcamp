a2a4dc9a6539ec78fe95da42d8b3104f14810460
escobard <escobard90@gmail.com>
2024-01-29 16:33:31 -0700

    section28: lessons 257, add timer to the tomato

--- section28/pomodoro/main.py
+++ section28/pomodoro/main.py
@@ -20,13 +20,16 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title("Pomodoro")
+window.config(padx=100, pady=50)
 
 # canvas with tkinter - allows us to draw an image on the screen
 canvas = Canvas(width=200, height=224)
 
 # photoimage - function with tkinter to read through an image file
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(100, 112, image=tomato_img)
+canvas.create_image(103, 112, image=tomato_img)
+canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+
 canvas.pack()
 
 window.mainloop()
