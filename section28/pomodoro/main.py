f22fc3cef249437f12b6cb5ceefdec07c645123c
escobard <escobard90@gmail.com>
2024-01-29 18:36:03 -0700

    section28: lessons 259, add a timer function

--- section28/pomodoro/main.py
+++ section28/pomodoro/main.py
@@ -12,29 +12,31 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+# calls the countodwn function to begin the timer
+def start_timer():
+  count_down(5)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 import time
 
-
-# ---------------------------- UI SETUP ------------------------------- #
-
-window = Tk()
-window.title("Pomodoro")
-window.config(padx=100, pady=50, bg=YELLOW)
-
-def say_something(count):
+def count_down(count):
   # change canvas to return new time
   canvas.itemconfig(timer_text, text=count)
   print(count)
   # prevents counts in the negative
   if count > 0:
     # .after calls a specified function after the specified time
-    window.after(1000, say_something, count - 1)
+    window.after(1000, count_down, count - 1)
 
+# ---------------------------- UI SETUP ------------------------------- #
 
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
 
 # canvas with tkinter - allows us to draw an image on the screen
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
@@ -47,11 +49,10 @@ tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 
-say_something(5)
 # for color palettes, can try https://colorhunt.co/
 canvas.grid(column=1, row=1)
 
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0,row=2)
 
 reset_button = Button(text="Reset", highlightthickness=0)
