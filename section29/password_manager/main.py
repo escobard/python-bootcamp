92c74b6cdbbf9cebaff66dc8f9242fd8bb52415b
escobard <escobard90@gmail.com>
2024-02-03 13:40:58 -0700

    section29: lessons 266, add entries

--- section29/password_manager/main.py
+++ section29/password_manager/main.py
@@ -16,11 +16,21 @@ canvas.create_image(100, 100, image=lock_image)
 canvas.grid(row=0, column=1)
 
 # Labels
-website_label = Label(text="Website", bg="white")
+website_label = Label(text="Website:", bg="white")
 website_label.grid(row=1, column=0)
-email_label = Label(text="Email/Username", bg="white")
+email_label = Label(text="Email/Username:", bg="white")
 email_label.grid(row=2, column=0)
-password_label = Label(text="Password", bg="white")
+password_label = Label(text="Password:", bg="white")
 password_label.grid(row=3, column=0)
+
+# Entries
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1)
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+#
 
 window.mainloop()
