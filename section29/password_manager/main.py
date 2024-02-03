6c024c0a8c1010b0baa8dd03e2ebdff3a1f63a94
escobard <escobard90@gmail.com>
2024-02-03 13:51:27 -0700

    section29: lessons 266, fix column spam and sticky to grid layouts

--- section29/password_manager/main.py
+++ section29/password_manager/main.py
@@ -6,7 +6,7 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password manager")
-window.config(padx=20, pady=20, bg="white")
+window.config(padx=40, pady=40, bg="white")
 
 canvas = Canvas(width=200, height=200, highlightthickness=0, bg="white")
 
@@ -17,24 +17,25 @@ canvas.grid(row=0, column=1)
 
 # Labels
 website_label = Label(text="Website:", bg="white")
-website_label.grid(row=1, column=0)
+## grid sticky property places elements to the left of the grid cell instead of the center
+website_label.grid(row=1, column=0, sticky=EW)
 email_label = Label(text="Email/Username:", bg="white")
-email_label.grid(row=2, column=0)
+email_label.grid(row=2, column=0, sticky=EW)
 password_label = Label(text="Password:", bg="white")
-password_label.grid(row=3, column=0)
+password_label.grid(row=3, column=0, sticky=EW)
 
 # Entries
 website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1)
+website_entry.grid(row=1, column=1, columnspan=2, sticky=EW)
 email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1)
-password_entry = Entry(width=21)
+email_entry.grid(row=2, column=1, columnspan=2, sticky=EW)
+password_entry = Entry(width=35)
 password_entry.grid(row=3, column=1)
 
 # Buttons
 generate_password_button = Button(text="Generate Password")
-generate_password_button.grid(row=3, column=2)
+generate_password_button.grid(row=3, column=2, sticky=EW)
 add_button = Button(text="Add", width=36)
-add_button.grid(row=4, column=1)
+add_button.grid(row=4, column=1, columnspan=2, sticky=EW)
 
 window.mainloop()
