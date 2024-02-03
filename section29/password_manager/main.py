3c161140599076234ddd2eb2d22b6550320f32c6
escobard <escobard90@gmail.com>
2024-02-03 13:38:02 -0700

    section29: lessons 266, add labels

--- section29/password_manager/main.py
+++ section29/password_manager/main.py
@@ -11,8 +11,16 @@ window.config(padx=20, pady=20, bg="white")
 canvas = Canvas(width=200, height=200, highlightthickness=0, bg="white")
 
 lock_image = PhotoImage(file="logo.png")
-canvas.create_image(100, 112, image=lock_image)
+canvas.create_image(100, 100, image=lock_image)
 
-canvas.grid(column=1, row=1)
+canvas.grid(row=0, column=1)
+
+# Labels
+website_label = Label(text="Website", bg="white")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username", bg="white")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password", bg="white")
+password_label.grid(row=3, column=0)
 
 window.mainloop()
