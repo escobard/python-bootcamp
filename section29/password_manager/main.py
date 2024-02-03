7bd7a61dbd764d3536a7696e0b6718da97b00a0c
escobard <escobard90@gmail.com>
2024-02-03 13:29:26 -0700

    section29: lessons 264, challenge, canvas with image background

--- section29/password_manager/main.py
+++ section29/password_manager/main.py
@@ -1,5 +1,18 @@
+from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password manager")
+window.config(padx=20, pady=20, bg="white")
+
+canvas = Canvas(width=200, height=200, highlightthickness=0, bg="white")
+
+lock_image = PhotoImage(file="logo.png")
+canvas.create_image(100, 112, image=lock_image)
+
+canvas.grid(column=1, row=1)
+
+window.mainloop()
