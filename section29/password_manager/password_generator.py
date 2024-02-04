e0b609ce7d9028c2da14a15e28cd87a48c598e60
escobard <escobard90@gmail.com>
2024-02-03 18:46:59 -0700

    section29: lessons 269, abbreviate solution by reducing lines of code

--- section29/password_manager/password_generator.py
+++ section29/password_manager/password_generator.py
@@ -1,25 +1,17 @@
 #Password Generator Project
-import random
+from random import randint, choice, shuffle
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
+# remove forloops by using list comprehension
+password_letters = [choice(letters) for _ in range(randint(8, 10))]
+password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
 
-password_list = []
+password_list = password_letters + password_symbols + password_numbers;
 
-for char in range(nr_letters):
-  password_list.append(random.choice(letters))
-
-for char in range(nr_symbols):
-  password_list += random.choice(symbols)
-
-for char in range(nr_numbers):
-  password_list += random.choice(numbers)
-
-random.shuffle(password_list)
+shuffle(password_list)
 
 password = ""
 for char in password_list:
