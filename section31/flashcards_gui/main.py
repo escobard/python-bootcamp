e32df66b3dc77fe70d0bdd4d8ee51b1dbe595762
escobard <escobard90@gmail.com>
2024-02-05 15:08:28 -0700

    section31: lesson 281, revise solution with instructor answer

--- section31/flashcards_gui/main.py
+++ section31/flashcards_gui/main.py
@@ -22,21 +22,27 @@ window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
-# required to create a canvas with a grid
-canvas.grid(row=0, column=1)
+
+# card image
+card_image = PhotoImage(file="./images/card_front.png")
+canvas.create_image(400, 263, image=card_image)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+
+# canvas text
+canvas.create_text(400, 150, font=("Ariel", 40, "italic"), text="French", fill="black")
+canvas.create_text(400, 263, font=("Ariel", 60, "bold"), text="trouve", fill="black")
+
+# columnspan is required to ensure flashcard object takes two columns' worth of space, otherwise buttons become misaligned
+canvas.grid(row=0, column=0, columnspan=2)
 
 # image buttons
-card_image = PhotoImage(file="./images/card_front.png")
-card_button = Button(image=card_image, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0)
-card_button.grid(row=0, column=1, columnspan=2)
 
 right_image = PhotoImage(file="./images/right.png")
-right_button = Button(image=right_image, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0)
-right_button.grid(row=1, column=2, sticky=EW)
+known_buton = Button(image=right_image, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0)
+known_buton.grid(row=1, column=0)
 
 wrong_image = PhotoImage(file="./images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0)
-wrong_button.grid(row=1, column=0, sticky=EW)
-
+unknown_button = Button(image=wrong_image, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0)
+unknown_button.grid(row=1, column=1)
 
 window.mainloop()
