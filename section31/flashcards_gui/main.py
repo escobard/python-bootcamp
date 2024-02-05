703f5bd52ab92aa408d7ad1cdd8c8c96fb0eb312
escobard <escobard90@gmail.com>
2024-02-05 16:48:32 -0700

    section31: lesson 282, add Flashcard class and load csv file's data with pandas

--- section31/flashcards_gui/main.py
+++ section31/flashcards_gui/main.py
@@ -1,5 +1,28 @@
+# STEP 2
+# 1. Read the data from the french_words.csv file in the data folder.
+#
+# 2. Pick a random French word/translation and put the word into the flashcard. Every time you press the ❌ or ✅ buttons, it should generate a new random word to display. e.g.
+import pandas
+
+
+class Flashcards:
+
+  def __init__(self):
+    self.language: str = ""
+    self.word: str = ""
+    self.words_dictionary: list[dict] = [{}]
+
+  def load_words(self):
+    words_file = pandas.read_csv("./data/french_words.csv")
+    self.words_dictionary = words_file.to_dict(orient="records")
+    print(self.words_dictionary)
+
+
+flashcards_store = Flashcards()
+
+# STEP 1
 # 1. Download the starting files from the course resources.
-# 2. Use the images in the image folder, to create the following user interface. The ❌ and ✅ are buttons. You can add images to buttons like this:
+# 2. Use the images in the image folder, to create the user interface. The ❌ and ✅ are buttons. You can add images to buttons like this:
 #
 # my_image = PhotoImage(file="path/to/image_file.png")
 # button = Button(image=my_image, highlightthickness=0)
@@ -38,11 +61,14 @@ canvas.grid(row=0, column=0, columnspan=2)
 # image buttons
 
 right_image = PhotoImage(file="./images/right.png")
-known_buton = Button(image=right_image, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0)
-known_buton.grid(row=1, column=0)
+known_button = Button(image=right_image, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0,
+                      command=flashcards_store.load_words)
+
+known_button.grid(row=1, column=1)
 
 wrong_image = PhotoImage(file="./images/wrong.png")
-unknown_button = Button(image=wrong_image, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0)
-unknown_button.grid(row=1, column=1)
+unknown_button = Button(image=wrong_image, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0,
+                        command=flashcards_store.load_words)
+unknown_button.grid(row=1, column=0)
 
 window.mainloop()
