b524f286e0251cc7cc428ea7cfc3992ec8d7f839
escobard <escobard90@gmail.com>
2024-02-05 17:30:32 -0700

    section31: lesson 284, reveal answer method

--- section31/flashcards_gui/main.py
+++ section31/flashcards_gui/main.py
@@ -34,6 +34,7 @@ class Flashcards:
   #
   # 2. The card image should change to the card_back.png and the text colour should change to white. The title of the card should change to "English" from "French".
   def reveal_answer(self):
+    window.after(3000, self.reveal_answer())
     self.language = "English"
     canvas.itemconfig(language_text, text=self.language)
     self.word = self.word_list[self.language]
@@ -58,13 +59,13 @@ class Flashcards:
 
 from tkinter import *
 
-flashcards_store = Flashcards()
-
 BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+
 
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 
@@ -74,11 +75,12 @@ canvas.create_image(400, 263, image=card_image)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 
 # canvas text
-language_text = canvas.create_text(400, 150, font=("Ariel", 40, "italic"), text=flashcards_store.language, fill="black")
-word_text = canvas.create_text(400, 263, font=("Ariel", 60, "bold"), text=flashcards_store.word, fill="black")
+language_text = canvas.create_text(400, 150, font=("Ariel", 40, "italic"), text="Title", fill="black")
+word_text = canvas.create_text(400, 263, font=("Ariel", 60, "bold"), text="Word", fill="black")
 
 # columnspan is required to ensure flashcard object takes two columns' worth of space, otherwise buttons become misaligned
 canvas.grid(row=0, column=0, columnspan=2)
+flashcards_store = Flashcards()
 
 # image buttons
 
