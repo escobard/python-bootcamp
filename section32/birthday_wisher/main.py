c2a743e2da1f3eaaf5da2f25bd3bde598b256f7c
escobard <escobard90@gmail.com>
2024-02-11 13:21:36 -0700

    section31: lesson 293, complete challenge

--- section32/birthday_wisher/main.py
+++ section32/birthday_wisher/main.py
@@ -8,6 +8,26 @@
 
 # 4. Send the letter generated in step 3 to that person's email address.
 
+# # instructor solution
+# from datetime import datetime
+# import pandas
+#
+# today = datetime.now
+#
+# ## build tupple to store today's date
+# today_tuple = (datetime.now().month, datetime.now().day)
+#
+# ## import csv data with pandas
+# data = pandas.read_csv("birthdays.csv")
+#
+# ## creates a new dictionary where each row has its own key
+# ### using pandas' data.iterrows() returns each row as its own dict
+# birthdays_dict = {
+#   ## creates a tuple key - that's new
+#   (data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()
+# }
+
+# my solution
 import pandas
 import datetime as dt
 import random
@@ -20,29 +40,33 @@ email_to = "escobardautomation@yahoo.com"
 
 ## extract birthday data from csv
 birthday_data = pandas.read_csv("birthdays.csv")
+birthday_data_rows = birthday_data.iterrows()
+# print(birthday_data_rows)
 
 birthday_list = birthday_data.to_dict()
 
 ## set today's date to check birthday data
 today = dt.datetime.now()
 
-if today.year in birthday_list["year"].values() and today.month in birthday_list["month"].values() and today.day in birthday_list["day"].values():
-  print('True!')
+for (index, data_row) in birthday_data_rows:
 
-  ## pick a random letter from the letter template
-  letters = ["./letter_templates/letter_1.txt", "./letter_templates/letter_2.txt", "./letter_templates/letter_3.txt"]
-  random_letter = random.choice(letters)
+  if today.year == data_row.year and today.month == data_row.month and today.day == data_row.day:
 
-  ## replace the [NAME] with the person's actual name
-  with open(random_letter) as letter:
-    data = letter.read().replace("[NAME]", "Name")
+    ## pick a random letter from the letter template
+    letters = ["./letter_templates/letter_1.txt", "./letter_templates/letter_2.txt", "./letter_templates/letter_3.txt"]
+    random_letter = random.choice(letters)
 
-    ## send the letter generated to that person's email address
-    with smtplib.SMTP("smtp.gmail.com", port=587) as gmail_connection:
-      gmail_connection.starttls()
-      gmail_connection.login(user=email, password=password)
-      gmail_connection.sendmail(
-        from_addr=email,
-        to_addrs=email_to,
-        msg=f"Subject:Happy birthday!\n\n{data}")
+    ## replace the [NAME] with the person's actual name
+    with open(random_letter) as letter:
+      ### using data_row["name"] instead of data_row.name - key value is returning incorrect value
+      data = letter.read().replace("[NAME]", data_row["name"])
+
+      ## send the letter generated to that person's email address
+      with smtplib.SMTP("smtp.gmail.com", port=587) as gmail_connection:
+        gmail_connection.starttls()
+        gmail_connection.login(user=email, password=password)
+        gmail_connection.sendmail(
+          from_addr=email,
+          to_addrs=data_row.email,
+          msg=f"Subject:Happy birthday!\n\n{data}")
 
