12e1f1305be3833d8f9bd2ccab5322ae8b3e841c
escobard <escobard90@gmail.com>
2024-02-11 12:22:51 -0700

    section31: lesson 292, improve with instructor answer

--- section32/main.py
+++ section32/main.py
@@ -1,34 +1,33 @@
-import random
+
 # challenge - send a motivational quote to the target email address every week on Tuesday
 
-## open file and store contents as list
-with open("quotes.txt") as quotes_file:
-  file_data = quotes_file.readlines()
-  # pick a random quote
-  random_quote = random.choices(file_data)
-  print(random_quote)
-
-## send email quote
+import datetime as dt
+import random
 import smtplib
+
+now = dt.datetime.now()
+day_of_week = now.weekday()
 
 email = "escobardtraining@gmail.com"
 password = "tpsx mkhh gfpp zxnf"
 
 email_to = "escobardautomation@yahoo.com"
 
-gmail_connection = smtplib.SMTP("smtp.gmail.com", port=587)
-
-gmail_connection.starttls()
-gmail_connection.login(user=email, password=password)
-
 ## check if day of the week is sunday, and send email if it is
-import datetime as dt
-
-now = dt.datetime.now()
-day_of_week = now.weekday()
-
 if day_of_week == 6:
-  gmail_connection.sendmail(from_addr=email, to_addrs=email_to, msg=f"Subject:Weekly quote\n\n{random_quote[0]}")
-  gmail_connection.close()
-else:
-  gmail_connection.close()
+
+  ## open file and store contents as list
+  with open("quotes.txt") as quotes_file:
+    file_data = quotes_file.readlines()
+    # pick a random quote
+    random_quote = random.choice(file_data)
+    print(random_quote)
+
+  ## send email quote
+  with smtplib.SMTP("smtp.gmail.com", port=587) as gmail_connection:
+    gmail_connection.starttls()
+    gmail_connection.login(user=email, password=password)
+    gmail_connection.sendmail(
+      from_addr=email,
+      to_addrs=email_to,
+      msg=f"Subject:Sunday motivation\n\n{random_quote}")
