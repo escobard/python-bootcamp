b2b531062e4b5623673e146463c3899716f6161b
escobard <escobard90@gmail.com>
2024-02-24 15:27:09 -0700

    section33: lesson 299, manipulate response data to store longitude and latitude tupple

--- section33/main.py
+++ section33/main.py
@@ -1,5 +1,16 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status() - raises an exception of status is not 200
+# response.status_code - returns the status call of the API
+# response.json() - returns the response in JSON format
+# response.json()['iss_position] - returns a specific object from the response
 
-print(response)
+data = response.json()
+
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (longitude, latitude)
+
+print(iss_position)
