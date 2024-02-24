d314bdbc54bf73414a075c43a956613c86c993ac
escobard <escobard90@gmail.com>
2024-02-24 15:53:03 -0700

    section33: lesson 301, api parameters for sunset and sunrise API

--- section33/kanye_quotes/main.py
+++ section33/kanye_quotes/main.py
@@ -8,7 +8,6 @@ def get_quote():
     quote = data["quote"]
     canvas.itemconfig(quote_text, text=quote)
 
-
 window = Tk()
 window.title("Kanye Says...")
 window.config(padx=50, pady=50)
@@ -22,7 +21,5 @@ canvas.grid(row=0, column=0)
 kanye_img = PhotoImage(file="kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
-
-
 
 window.mainloop()
