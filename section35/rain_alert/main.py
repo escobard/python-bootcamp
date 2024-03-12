c2c85bfa365d43d041b1553ec44af1dc9c5c3542
escobard <escobard90@gmail.com>
2024-03-12 17:22:16 -0600

    section35, lesson 315, send alert if weather conditions indicate a chance of rain

--- section35/rain_alert/main.py
+++ section35/rain_alert/main.py
@@ -6,12 +6,25 @@ api_key = "82a5bd518c48cfee0874b684b5c86dca"
 
 parameters = {
   "lat": 51.0447,
-  "lon": 114.0719,
-  "appid": api_key
+  "lon": -0.127758,
+  "appid": api_key,
+  "cnt": 4
 }
 
 response = requests.get(url=owm_endpoint, params=parameters)
 
 response.raise_for_status()
 weather_data = response.json()
-print(weather_data)
+print(weather_data["list"][0]["weather"][0]["id"])
+
+
+will_rain = False
+for hour_data in weather_data["list"]:
+  condition_code = hour_data["weather"][0]["id"]
+  print(condition_code)
+  # if weather api data has an id less than 700, there is a chance of rain
+  if int(condition_code) < 700:
+    will_rain = True
+
+if will_rain:
+  print("Bring an umbrella.")
