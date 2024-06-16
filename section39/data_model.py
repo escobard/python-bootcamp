c29347b568c23f69445ae41965d2b5067608b8e9
escobard <escobard90@gmail.com>
2024-06-16 09:36:57 -0600

    section39, capstone, data model types

--- section39/data_model.py
+++ section39/data_model.py
@@ -1,5 +1,18 @@
+flight_thresholds_type = set[list[str]]
+flight_search_type = dict[dict[str, str]]
+flight_matches_type = set[list[str]]
+
 class DataModel:
   def __init__(self):
-    self.flight_thresholds: set[list[str]] | None
-    self.flight_search: dict[dict[str, str]] | None
-    self.flight_matches: set[list[str]] | None
+    self.flight_thresholds: flight_thresholds_type | None = None
+    self.flight_search: flight_search_type | None = None
+    self.flight_matches: flight_matches_type | None = None
+
+  def get_flight_thresholds(self) -> flight_thresholds_type:
+    return self.flight_thresholds
+
+  def set_flight_thresholds(self, flight_thresholds: flight_thresholds_type) -> None:
+    self.flight_thresholds = flight_thresholds
+
+  def get_flight_search(self):
+    return
