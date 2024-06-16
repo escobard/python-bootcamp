4d5c92e8cac77813ccd7699abeaf190ad96a52c0
escobard <escobard90@gmail.com>
2024-06-16 09:18:57 -0600

    section39, capstone, document steps

---
+++ section39/main.py
@@ -0,0 +1,5 @@
+# find amazing flight deals
+## extra challenge - build using OOP
+## step 1. set up a google sheet with price cut offs and the locations we want to go to
+## step 2. match google sheet data with current flight deals
+## step 3. send sns on flight deal matches
