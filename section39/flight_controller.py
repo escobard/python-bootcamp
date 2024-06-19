cb6f185c4c381ed5200b053c0fa92b52eebd36e4
escobard <escobard90@gmail.com>
2024-06-19 07:12:54 -0600

    section39, capstone, add amadeus env keys and auth headers to FlightController

--- section39/flight_controller.py
+++ section39/flight_controller.py
@@ -1,7 +1,10 @@
 import os
 import requests
+from dotenv import load_dotenv
 
 from data_model import DataModel
+
+load_dotenv()
 
 
 class FlightController:
@@ -15,6 +18,19 @@ class FlightController:
       'Authorization': f'Basic {self.SHEETY_TOKEN}'
     }
     self.sheety_endpoint: str = f'https://api.sheety.co/{self.SHEETY_API_KEY}/flights/prices'
+
+    self.AMADEUS_API_KEY: str = os.environ.get('AMADEUS_API_KEY')
+    self.AMADEUS_API_SECRET: str = os.environ.get('AMADEUS_API_SECRET')
+
+    self.amadeus_auth_headers: dict[str, str] = {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    self.body = {
+      'grant-type': 'client_credentials',
+      'client_id': self.AMADEUS_API_KEY,
+      'client_secret': self.AMADEUS_API_SECRET
+    }
 
   def fetch_flight_thresholds(self):
     sheety_request = requests.get(url=self.sheety_endpoint, headers=self.sheety_headers)
