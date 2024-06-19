b3102681a71e2f384f8d47c77a3dbe10f1a64e44
escobard <escobard90@gmail.com>
2024-06-19 16:07:33 -0600

    section39, capstone, add base flight criteria data

--- section39/data_model.py
+++ section39/data_model.py
@@ -2,11 +2,53 @@ flight_thresholds_type = dict[str, [list[dict[str:'city', str:'iataCode', str:'i
 flight_search_type = set[list[str]]
 flight_matches_type = set[list[str]]
 
+
 class DataModel:
   def __init__(self):
     self.flight_thresholds: flight_thresholds_type | None = None
     self.flight_search: flight_search_type | None = None
     self.flight_matches: flight_matches_type | None = None
+
+    # https://developers.amadeus.com/self-service/category/flights/api-doc/flight-offers-search/api-reference
+    self.flight_search_criteria = {
+      "currencyCode": "CAD",
+      "originDestinations": [
+        # populate this with data from flight threshold
+        # {
+        #   "id": "1",
+        #   "originLocationCode": "NYC",
+        #   "destinationLocationCode": "MAD",
+        #   "departureDateTimeRange": {
+        #     "date": "2023-11-01",
+        #     "time": "10:00:00"
+        #   }
+        # }
+      ],
+      "travelers": [
+        {
+          "id": "1",
+          "travelerType": "ADULT"
+        }
+      ],
+      "sources": [
+        "GDS"
+      ],
+      "searchCriteria": {
+        "maxFlightOffers": 2,
+        "flightFilters": {
+          "cabinRestrictions": [
+            {
+              "cabin": "BUSINESS",
+              "coverage": "MOST_SEGMENTS",
+              "originDestinationIds": [
+                # populate this with total number of destination Ids from flight threshold
+                # "1"
+              ]
+            }
+          ]
+        }
+      }
+    }
 
   def get_flight_thresholds(self) -> flight_thresholds_type:
     return self.flight_thresholds
