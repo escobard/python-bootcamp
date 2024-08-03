c937bf02834f60054a9f0efc74d163dd66b699a8
escobard <escobard90@gmail.com>
2024-08-03 09:55:13 -0600

    section40, add method to loop through flight matches & user emails, then send email alert

--- section40/notification_controller.py
+++ section40/notification_controller.py
@@ -53,13 +53,16 @@ class NotificationController:
       print(sms_body)
       self.send_sms(sms_body)
 
-  def send_email(self, body: str) -> None:
+  def send_email(self, body: str, to_address: str) -> None:
     gmail_connection = smtplib.SMTP("smtp.gmail.com", port=587)
-    # establishes connection with provider
     gmail_connection.starttls()
     gmail_connection.login(user=self.smtp_email, password=self.smtp_password)
-    # email without a subject is likely to be flagged as spam!
-    gmail_connection.sendmail(from_addr=self.smtp_email, to_addrs=self.smtp_password, msg=body)
-    # closes the connection to smtp client
+    gmail_connection.sendmail(from_addr=self.smtp_email, to_addrs=to_address, msg=body)
     gmail_connection.close()
+
+  def send_flight_matches_emails(self) -> None:
+    for flight_match in self.model.get_flight_matches():
+      email_body: str = f"Low price alert! Only ${flight_match['price']} CAD to fly from {flight_match['origin_airport']} to {flight_match['destination_airport']}, on {flight_match['out_date']}."
+      for user_email in self.model.get_user_emails():
+        self.send_email(email_body, user_email)
 
