1e16b2bf660486b2302634790c68bc328c5901d3
escobard <escobard90@gmail.com>
2024-08-03 09:44:53 -0600

    section40, add env variables and method to send email with smtp

--- section40/notification_controller.py
+++ section40/notification_controller.py
@@ -1,4 +1,5 @@
 import os
+import smtplib
 from dotenv import load_dotenv
 from twilio.rest import Client
 
@@ -26,6 +27,8 @@ class NotificationController:
     self.twilio_auth_token: str = os.environ.get('TWILIO_AUTH_TOKEN')
     self.twilio_virtual_number: str = os.environ.get('TWILIO_VIRTUAL_NUMBER')
     self.twilio_verified_number: str = os.environ.get('TWILIO_VERIFIED_NUMBER')
+    self.smtp_email: str = os.environ.get('SMTP_EMAIL')
+    self.smtp_password: str = os.environ.get('SMTP_PASSWORD')
 
   def send_sms(self, body: str) -> None:
     """
@@ -35,7 +38,7 @@ class NotificationController:
         body (str): The body of the SMS message to be sent.
     """
     client = Client(self.twilio_id, self.twilio_auth_token)
-    message = client.messages.create(
+    client.messages.create(
       from_=self.twilio_virtual_number,
       body=body,
       to=self.twilio_verified_number
@@ -49,3 +52,14 @@ class NotificationController:
       sms_body: str = f"Low price alert! Only ${flight_match['price']} CAD to fly from {flight_match['origin_airport']} to {flight_match['destination_airport']}, on {flight_match['out_date']}."
       print(sms_body)
       self.send_sms(sms_body)
+
+  def send_email(self, body: str) -> None:
+    gmail_connection = smtplib.SMTP("smtp.gmail.com", port=587)
+    # establishes connection with provider
+    gmail_connection.starttls()
+    gmail_connection.login(user=self.smtp_email, password=self.smtp_password)
+    # email without a subject is likely to be flagged as spam!
+    gmail_connection.sendmail(from_addr=self.smtp_email, to_addrs=self.smtp_password, msg=body)
+    # closes the connection to smtp client
+    gmail_connection.close()
+
